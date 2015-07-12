3a63592814e0f8b7c3d8e30a6cdae8e757b47923
Nicholas Hebb <nhebb@breezetree.com>
2015-07-12 01:24:53 -0700

    Walkthrough completed
    Added form, form validation, bootstrap, and email response.

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -14,6 +15,26 @@ namespace PartyInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good morning" : "Good evening";
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // There is a validation error
+                return View();
+            }
         }
     }
 }
